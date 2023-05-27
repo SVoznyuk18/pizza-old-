1a9f563bdadce583ff2c646e827993837678a9ae
Sergey.V <s.voznyuk18@gmail.com>
2023-05-27 23:06:41 +0300

    fix eslint error LabelComponent

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -1,33 +1,37 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Label } from './StyledComponents';
 
-const LabelComponent = ({ label, htmlFor, labelFontSize, labelMarginBottom, labelLineHeight }) => {
-    return (
-        <Label
-            htmlFor={htmlFor}
-            labelFontSize={labelFontSize}
-            labelMarginBottom={labelMarginBottom}
-            labelLineHeight={labelLineHeight}
-        >
-            {label}
-        </Label>
-    );
-}
+const LabelComponent = ({
+  label,
+  htmlFor,
+  labelFontSize,
+  labelMarginBottom,
+  labelLineHeight,
+}) => (
+  <Label
+    htmlFor={htmlFor}
+    labelFontSize={labelFontSize}
+    labelMarginBottom={labelMarginBottom}
+    labelLineHeight={labelLineHeight}
+  >
+    {label}
+  </Label>
+);
 
 LabelComponent.propTypes = {
-    label: PropTypes.string.isRequired,
-    htmlFor: PropTypes.string.isRequired,
-    labelFontSize: PropTypes.string,
-    labelMarginBottom: PropTypes.string,
-    labelLineHeight: PropTypes.string
-}
+  label: PropTypes.string.isRequired,
+  htmlFor: PropTypes.string.isRequired,
+  labelFontSize: PropTypes.string,
+  labelMarginBottom: PropTypes.string,
+  labelLineHeight: PropTypes.string,
+};
 
 LabelComponent.defaultProps = {
-    labelFontSize: '16px',
-    labelMarginBottom: '5px',
-    labelLineHeight: '16px'
-}
+  labelFontSize: '16px',
+  labelMarginBottom: '5px',
+  labelLineHeight: '16px',
+};
 
 export default LabelComponent;
--- src/components/Label/StyledComponents.js
+++ src/components/Label/StyledComponents.js
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
-import { media } from "ConfigsRoot/constants";
+import { media } from 'ConfigsRoot/constants';
 
 export const Label = styled.div`
     display: inline;
-    font-size: ${props => props.labelFontSize};
-    margin-bottom: ${props => props.labelMarginBottom};
-    line-height: ${props => props.labelLineHeight};
+    font-size: ${(props) => props.labelFontSize};
+    margin-bottom: ${(props) => props.labelMarginBottom};
+    line-height: ${(props) => props.labelLineHeight};
     font-weight: 600;
 
     ${media.mobile} {
