2f1509d01c560dcf250226635fce588f48a3f35b
Sergey.V <s.voznyuk18@gmail.com>
2023-06-14 22:27:09 +0300

    fix eslint errors inside Table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,45 +1,78 @@
-import React, { memo } from "react";
+/* eslint-disable import/no-cycle */
+import React, { memo } from 'react';
+import PropTypes from 'prop-types';
 
-import { TableContent, TableHeader, TableHeaderItem, TableRow, TableRowItem } from './StyledComponents';
-import { SvgIcon, Img } from 'ComponentsRoot';
 import iconSvg from 'AssetsRoot/svg/iconSvg';
 import defaultAvatar from 'AssetsRoot/img/defaultAvatar.png';
 import loadingImg from 'AssetsRoot/svg/spinerLoading.svg';
-import { colors } from "ConfigsRoot/colors"
+import { SvgIcon, Img } from 'ComponentsRoot';
+import {
+  TableContent,
+  TableHeader,
+  TableHeaderItem,
+  TableRow,
+  TableRowItem,
+} from './StyledComponents';
 
-const Table = ({ tableData, handleDelete, header }) => {
-    return (
-        <TableContent>
-            <TableHeader>
-                {header && header?.map((item, index) => (
-                    <TableHeaderItem key={index}>{item}</TableHeaderItem>
-                ))}
-            </TableHeader>
-            {
-                tableData && tableData.map(item => (
-                    <TableRow key={item?.id}>
-                        <TableRowItem>{item?.name}</TableRowItem>
-                        <TableRowItem>{item?.email}</TableRowItem>
-                        <TableRowItem>{item?.phone}</TableRowItem>
-                        <TableRowItem><Img width='35px' height='35px' borderRadius='50%' src={item?.avatarUrl} loadingImg={loadingImg} errorImg={defaultAvatar} alt='avatar' /></TableRowItem>
-                        <TableRowItem>{item?.role}</TableRowItem>
-                        <TableRowItem>
-                            <SvgIcon
-                                width='20px'
-                                height='20px'
-                                viewBox="0 0 20 20"
-                                path={iconSvg.trash}
-                                fill="#fff"
-                                strokeHover="#373737"
-                                stroke="#B6B6B6"
-                                handleClick={() => handleDelete(item?.id)}
-                            />
-                        </TableRowItem>
-                    </TableRow>
-                ))
-            }
-        </TableContent>
-    )
-}
+const Table = ({ tableData, handleDelete, header }) => (
+  <TableContent>
+    <TableHeader>
+      {header && header?.map((item) => (
+        <TableHeaderItem key={item}>{item}</TableHeaderItem>
+      ))}
+    </TableHeader>
+    {
+      tableData && tableData.map((item) => (
+        <TableRow key={item?.id}>
+          <TableRowItem>{item?.name}</TableRowItem>
+          <TableRowItem>{item?.email}</TableRowItem>
+          <TableRowItem>{item?.phone}</TableRowItem>
+          <TableRowItem>
+            <Img
+              width="35px"
+              height="35px"
+              borderRadius="50%"
+              src={item?.avatarUrl}
+              loadingImg={loadingImg}
+              errorImg={defaultAvatar}
+              alt="avatar"
+            />
+          </TableRowItem>
+          <TableRowItem>{item?.role}</TableRowItem>
+          <TableRowItem>
+            <SvgIcon
+              width="20px"
+              height="20px"
+              viewBox="0 0 20 20"
+              path={iconSvg.trash}
+              fill="#fff"
+              strokeHover="#373737"
+              stroke="#B6B6B6"
+              handleClick={() => handleDelete(item?.id)}
+            />
+          </TableRowItem>
+        </TableRow>
+      ))
+    }
+  </TableContent>
+);
+
+Table.propTypes = {
+  tableData: PropTypes.arrayOf(PropTypes.shape({
+    avatarUrl: PropTypes.string,
+    email: PropTypes.string,
+    id: PropTypes.string,
+    name: PropTypes.string,
+    password: PropTypes.string,
+    phone: PropTypes.string,
+    role: PropTypes.string,
+  })).isRequired,
+  header: PropTypes.arrayOf(PropTypes.string).isRequired,
+  handleDelete: PropTypes.func,
+};
+
+Table.defaultProps = {
+  handleDelete: () => {},
+};
 
 export default memo(Table);
