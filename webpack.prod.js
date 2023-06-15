9829b73f21f40f9a2460102428217b09a1bcf91c
Sergey.V <s.voznyuk18@gmail.com>
2023-06-15 14:04:19 +0300

    add BundleAnalyzerPlagin, add splitChanks to webpack.prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -19,9 +19,25 @@ const prodConfig = {
   optimization: {
     minimize: true,
     splitChunks: {
-      chunks: 'all',
-      minSize: 1,
-      minChunks: 2,
+      chunks: 'async',
+      minSize: 20000,
+      minRemainingSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      enforceSizeThreshold: 50000,
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+          reuseExistingChunk: true,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
     },
     minimizer: [
       new TerserPlugin({
@@ -30,6 +46,7 @@ const prodConfig = {
           ecma: 2015,
           compress: true,
           sourceMap: false,
+          mangle: true,
         },
       }),
       new CssMinimizerPlugin(),
